e4d33ca04739b9dd4c8e83767ca53c8b0001253a
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-02-21 17:26:23 +0530

    update schema sql file to add primary key

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 create table animals ( 
-    id int, 
+    id int serial primary key, 
     name text, 
     date_of_birth date, 
     escape_attempts int, 
